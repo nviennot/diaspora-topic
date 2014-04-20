fd37ae945bee9d8dd238a23261494f6db7e3a76d
Nicolas Viennot <nicolas@viennot.biz>
2014-04-20 01:04:35 -0400

    configure promiscuous for gantt charts

--- config/initializers/promiscuous.rb
+++ config/initializers/promiscuous.rb
@@ -1,7 +1,7 @@
 Promiscuous.configure do |config|
   config.publisher_exchange   = 'diaspora-topic'
   config.subscriber_exchanges = ['diaspora']
-
-  config.logger = Logger.new(STDERR)
-  config.logger.level = 0
+  config.logger = Logger.new(STDERR).tap { |l| l.level = 0 }
+  config.instrumentation_file = '/tmp/instrumentation.log'
+  config.redis_url            = 'redis://localhost:7777/'
 end
